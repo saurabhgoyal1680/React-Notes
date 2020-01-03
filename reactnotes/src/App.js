4f68c1f9fdf83299790316be6a681147fc1721c7
saurabh.goyal <saurabh.goyal@servicenow.com>
2020-01-03 08:57:51 +0530

    create-react-app

---
+++ reactnotes/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {} from 'antd';
+import 'antd/dist/antd.css';
+
+class App extends React.Component{
+	render(){
+		return(
+			<div>
+
+			</div>
+		)
+	}
+}
+
+export default App;
