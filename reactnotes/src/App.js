d09e41a94a6e117739c850d3abf90509f2209982
saurabhgoyal1680 <59084561+saurabhgoyal1680@users.noreply.github.com>
2020-01-22 20:31:32 +0530

    FrontEnd part 2 (#2)

--- reactnotes/src/App.js
+++ reactnotes/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import NewNote from './NewNote';
-import {Layout, Icon, Input, Button} from 'antd';
+import NewFolder from './NewFolder';
+import Client from './Client'
+import {Layout, Icon, Input, Button, Modal} from 'antd';
 import swal from '@sweetalert/with-react'
 
 const {Header} =Layout;
@@ -12,13 +14,34 @@ class App extends React.Component{
 		super(props);
 		this.formRef = null;
 		this.state={
-			newNote : false
+			showModal : false,
+			showNewNoteForm: false,
+			showNewFolderForm: false,
+			modalTitle: <></>
 		}
 	}
 
 	showModal(){
 		this.setState({
-			visible: true
+			showModal: true,
+			showNewNoteForm: true,
+			modalTitle: <div><Icon type="diff"/>New Note</div>
+		})
+	}
+
+	showNewNoteForm(){
+		this.setState({
+			showNewNoteForm: true,
+			showNewFolderForm: false,
+			modalTitle: <div><Icon type="diff"/>New Note</div>
+		})
+	}
+
+	showNewFolderForm(){
+		this.setState({
+			showNewFolderForm: true,
+			showNewNoteForm: false,
+			modalTitle: <div><Icon type="folder-add"/>New Folder</div>
 		})
 	}
 
@@ -28,20 +51,27 @@ class App extends React.Component{
 		if (err) {
 			return;
 		}
-
+		if(values.hasOwnProperty('favourite') && values.favourite === undefined)
+			values.favourite = false;
 		console.log('Received values of form: ', values);
+
 		form.resetFields();
+		let text = "Successfully Noted";
+		if(this.state.showNewFolderForm)
+			text = "Folder creation successful";
 		swal({
-			text: "Successfully Noted",
+			text: text,
 			icon: "success"
 		})
-		this.setState({ visible: false });
+		this.handleCancel();
 		});
 	}
 
 	handleCancel(){
 		this.setState({
-			visible: false
+			showModal: false,
+			showNewNoteForm: true,
+			showNewFolderForm: false,
 		})
 	}
 
@@ -63,12 +93,35 @@ class App extends React.Component{
 						<Button type="primary" icon="plus" onClick={this.showModal.bind(this)}></Button>
 						<Search placeholder="Search your note here..." id="searchBox" style={{width:"50%"}} enterButton></Search>
 					</Input.Group>
-					<NewNote 
-						wrappedComponentRef={this.saveFormRef}
-						visible={this.state.visible}
-						onOk={this.handleOk.bind(this)}
-						onCancel={this.handleCancel.bind(this)}>
-					</NewNote>
+					<Modal
+						visible={this.state.showModal}
+						onCancel={this.handleCancel.bind(this)}
+						title={this.state.modalTitle}
+						footer={[
+							<Button key="ok" type="submit" onClick={this.handleOk.bind(this)}>Ok</Button>,
+							<Button key="cancel" onClick={this.handleCancel.bind(this)}>Cancel</Button>
+						]}
+					>
+						<Button.Group>
+							<Button autoFocus onClick={this.showNewNoteForm.bind(this)}>New Note</Button>
+							<Button onClick={this.showNewFolderForm.bind(this)}>New Folder</Button>
+						</Button.Group>
+						{this.state.showNewFolderForm ?
+						<NewFolder 
+							wrappedComponentRef={this.saveFormRef}
+							visible={this.state.showNewFolderForm ? "block" : "none"}
+							onSubmit={this.handleOk.bind(this)}
+							onCancel={this.handleCancel.bind(this)}>
+						</NewFolder> :
+						<NewNote 
+							wrappedComponentRef={this.saveFormRef}
+							visible={this.state.showNewNoteForm ? "block" : "none"}
+							onSubmit={this.handleOk.bind(this)}
+							onCancel={this.handleCancel.bind(this)}>
+						</NewNote>
+						}
+					</Modal>
+					
 				</div>
 			</div>
 		)
